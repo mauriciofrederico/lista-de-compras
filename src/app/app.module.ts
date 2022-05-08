1b711c237e44803e106e760f974a69ba643867c5
Mauricio Frederico <maufrederico@yahoo.com.br>
2022-05-08 12:51:09 -0300

    Criado 3 componentes: cabecalho, menu e rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
